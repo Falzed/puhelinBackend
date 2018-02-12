04f9d1f218610441c8ddcdd0f79f3ef481cda806
Falzed <oskari.kulmala@helsinki.fi>
2018-02-12 15:18:50 +0200

    3.12

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ const app = express()
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const cors = require('cors')
+const mongoose = require('mongoose')
+const fs = require('fs')
+
+const password = fs.readFileSync('password.config').toString().replace('\n', '')
+const username = fs.readFileSync('username.config').toString().replace('\n', '')
+
+const url =
+    "mongodb://".concat(username).concat(":").concat(password)
+    .concat("@ds129428.mlab.com:29428/fs-3-db") 
+
+mongoose.connect(url)
 
 morgan.token('url', (req, res) => {
     return JSON.stringify(req.body)
